73e4f515a75cdae9b8a7407b320a7db2616b257f
Tomas Karlik <tomas.karlik@gmail.com>
2025-01-24 22:46:08 +0100

    init

---
+++ RUIANDownloader/Interfaces/Models/IDownloaderSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace RUIANDownloader.Interfaces.Models
+{
+
+    public interface IDownloaderSettings
+    {
+
+        /// <summary>
+        /// CZUK Atom Service - RUIAN-CSV-ADR-OB
+        /// </summary>
+        string AtomServiceURL { get; set; }
+
+
+        int MaxNumberOfDownloadAttempts { get; set; }
+
+
+        /// <summary>
+        /// Delay between unsuccessful attempts in ms
+        /// </summary>
+        int RetryDownloadDelay { get; set; }
+
+    }
+
+}
---
+++ RUIANDownloader/Models/DownloaderSettings.cs
@@ -0,0 +1,19 @@
+﻿using RUIANDownloader.Interfaces.Models;
+
+namespace RUIANDownloader.Models
+{
+
+    public class DownloaderSettings : IDownloaderSettings
+    {
+
+        public string AtomServiceURL { get; set; } = "https://atom.cuzk.cz/getservicefeed.ashx?service=RUIAN-CSV-ADR-OB";
+
+
+        public int MaxNumberOfDownloadAttempts { get; set; } = 3;
+
+
+        public int RetryDownloadDelay { get; set; } = 1500;
+
+    }
+
+}
